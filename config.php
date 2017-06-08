0dda01fb404f8a82fc65c919807188037f40afe9
Bill Ferster <bferster@virginia.edu>
2017-06-08 16:56:54 -0400

    mysql

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbms = 'mysql';
 $dbhost = 'localhost';
 $dbname = 'ctscan';
 $dbuser = 'ctscan';
-$dbpasswd = 'MKctscan2017';
+$dbpasswd = '';
 $connection = mysql_connect($dbhost, $dbuser, $dbpasswd) or die ("Could not connect to server.");
 $dc = mysql_select_db($dbname,$connection) or die ("Couldn't select database.");
 ?>
