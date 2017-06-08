09b32410338e93ebf66c8686561cf1eba23c3baa
Bill Ferster <bferster@virginia.edu>
2017-06-08 16:56:03 -0400

    mysql

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-// configuration file for qmedia
+// configuration file for CTScan
 
 $dbms = 'mysql';
 $dbhost = 'localhost';
 $dbname = 'ctscan';
 $dbuser = 'ctscan';
-$dbpasswd = 'mkctscan2017';
+$dbpasswd = 'MKctscan2017';
 $connection = mysql_connect($dbhost, $dbuser, $dbpasswd) or die ("Could not connect to server.");
 $dc = mysql_select_db($dbname,$connection) or die ("Couldn't select database.");
 ?>
---
+++ saveevent.php
@@ -0,0 +1,51 @@
+<?php
+header('Cache-Control: no-cache, no-store, must-revalidate'); 
+header('Expires: Sun, 01 Jul 2005 00:00:00 GMT'); 
+header('Pragma: no-cache'); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Max-Age: 1000');
+require_once('config.php');
+			
+	$ver=0;														// Version number									
+	$obsId=$_REQUEST['obsId'];									// Get obsId
+	$time=$_REQUEST['time'];									// Get time
+	$d1=$_REQUEST['d1'];										// Get data
+	$d2=$_REQUEST['d2'];										// Get data
+	$d3=$_REQUEST['d3'];										// Get data
+	$d4=$_REQUEST['d4'];										// Get data
+	$d5=$_REQUEST['d5'];										// Get data
+		
+	$query="INSERT INTO qshow (obsId, time, d1, d2, d3, d4, d5) VALUES ('";
+		$query.=addEscapes($obsId)."','";
+		$query.=addEscapes($time)."','";
+		$query.=addEscapes($d1)."','";
+		$query.=addEscapes($d2)."','";
+		$query.=addEscapes($d3)."','";
+		$query.=addEscapes($d4)."','";
+		$query.=addEscapes($d5)."')";
+		$result=mysql_query($query);							// Add row
+		if ($result == false)									// Bad save
+			print("-2");										// Show error 
+		else
+			print(mysql_insert_id()."\n");						// Return ID of new resource
+		}
+		if ($result == false)									// Bad update
+			print("-4");										// Show error 
+		else
+			print($id);											// Show id 
+			}													// End if valid
+		mysql_close();											// Close session
+
+	
+	function addEscapes($str)									// ESCAPE ENTRIES
+	{
+		if (!$str)												// If nothing
+			return $str;										// Quit
+		$str=mysql_real_escape_string($str);					// Add slashes
+		$str=str_replace("\r","",$str);							// No crs
+		return $str;
+	}
+
+?>
+	
