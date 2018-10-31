8f6af07eeb5d991b849b51f1fc2df477e732ff5a
Bill Ferster <bferster@virginia.edu>
2018-10-31 16:10:38 -0400

    limit on queries

--- query.php
+++ query.php
@@ -7,23 +7,25 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Max-Age: 1000');
 require_once('vconfig.php');
 
-$q=mysqli_real_escape_string($link,$_GET['q']);			// Get q
-$q=str_replace("~","'","$q");							// Turn ~ into '
-$query="SELECT * FROM sessions WHERE ".$q; 				// Make query string
-if (strstr($q,"SELECT"))								// If already has a SELECT
-	$query=$q;											// Use it whole
-$result=mysqli_query($link, $query);					// Run query
-$myArray="";
-
-if ($result == false)									// Bad query
-	print(mysqli_error($link));							// Show error 
-else{													// Good query
-  	while ($row=$result->fetch_array(MYSQL_ASSOC)) 		// Get data
-        $myArray[]=$row;								// Add to array
-    if ($myArray)										// If valid
-		echo json_encode($myArray);						// Save as JSON
-	mysqli_free_result($result);						// Free
- 	}
+	$q=mysqli_real_escape_string($link,$_GET['q']);			// Get q
+	$q=str_replace("~","'","$q");							// Turn ~ into '
+	$query="SELECT * FROM sessions WHERE ".$q; 				// Make query string
+	if (strstr($q,"SELECT"))								// If already has a SELECT
+		$query=$q;											// Use it whole
+	$query=$query."  LIMIT 30";
+	$result=mysqli_query($link, $query);					// Run query
+	$myArray=array();										// Array to hold results
+	if ($result == false)									// Bad query
+		print(mysqli_error($link));							// Show error 
+	else{													// Good query
+		while ($row=$result->fetch_array(MYSQL_ASSOC)) 		// Get data
+			$myArray[]=$row;								// Add to array
+		echo json_encode($myArray);							// Save as JSON
+		mysqli_free_result($result);						// Free
+		}
 
 mysqli_close($link);									// Close session
 ?>
+
+
+
